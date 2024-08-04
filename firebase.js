3b8f51c4913130d81e339c1286de686995f2f2d9
Saad Inam <saadinam.mn@gmail.com>
2024-08-04 20:19:44 +0500

    Fixed the login issue

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB9pwS-GOOzHHrGQJ6TBJjqKvve8NINOSc",
+  apiKey: process.env.apiKey,
   authDomain: "storage-organizer.firebaseapp.com",
   projectId: "storage-organizer",
   storageBucket: "storage-organizer.appspot.com",
-  messagingSenderId: "302161098781",
-  appId: "1:302161098781:web:f8bca0b71056d8d2f2eef4",
-  measurementId: "G-TN9WHJWDTK"
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
