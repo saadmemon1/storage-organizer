d635d7ae8d40e51abad9baa008e5269ff8b6c257
Saad Inam <saadinam.mn@gmail.com>
2024-08-04 20:57:44 +0500

    Updated landing page

--- app/landing/page.js
+++ app/landing/page.js
@@ -1,25 +1,46 @@
 "use client";
 import * as React from 'react';
 import { useRouter } from 'next/navigation';
-import { Typography, Button, Container, Box } from '@mui/material';
+import { Typography, Button, Container, Box, Link } from '@mui/material';
+import StorageIcon from '@mui/icons-material/Storage';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { styled } from '@mui/system';
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+  height: '100vh', // Full view height
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center', // Center content vertically
+  alignItems: 'center', // Center content horizontally
+}));
 
 const LandingPage = () => {
-    const router = useRouter();
-    return (
-        <Container maxWidth="sm">
-            <Box textAlign="center" mt={5}>
-                <Typography variant="h2" gutterBottom>
-                    Welcome to Storage Organizer
-                </Typography>
-                <Typography variant="subtitle1" gutterBottom>
-                    Ready to experience the next level of organization? Get all your storage needs sorted with ease and efficiency!
-                </Typography>
-                <Button variant="contained" color="primary" onClick={() => router.push('/signin')}>
-                    Get Started
-                </Button>
-            </Box>
-        </Container>
-    );
+  const router = useRouter();
+  return (
+    <StyledContainer maxWidth="sm">
+      <Box textAlign="center" sx={{ width: '100%' }}>
+        <StorageIcon color="primary" sx={{ fontSize: 60, marginBottom: 2 }} />
+        <Typography variant="h2" gutterBottom>
+          Welcome to Storage Organizer
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          Ready to experience the next level of organization? Get all your storage needs sorted with ease and efficiency!
+        </Typography>
+        <Button variant="contained" color="primary" onClick={() => router.push('/signin')} sx={{ marginTop: 3 }}>
+          Get Started
+        </Button>
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+          <Link href="https://github.com/saadmemon1" target="_blank" color="inherit">
+            <GitHubIcon sx={{ fontSize: 40, mx: 2 }} />
+          </Link>
+          <Link href="https://linkedin.com/in/saadinamm" target="_blank" color="inherit">
+            <LinkedInIcon sx={{ fontSize: 40, mx: 2 }} />
+          </Link>
+        </Box>
+      </Box>
+    </StyledContainer>
+  );
 };
 
 export default LandingPage;
